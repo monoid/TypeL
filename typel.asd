4d77c33d76c7eb77cb7f5e4d47da6baa9d2da58a
Ivan Boldyrev <lispnik@gmail.com>
2022-03-30 13:55:30 +0700

    Use MIT License

--- typel.asd
+++ typel.asd
@@ -2,7 +2,7 @@
   :depends-on ()
   :version "0.1.1"
   :author "Ivan Boldyrev <lispnik@gmail.com>"
-  :license "Lisp Lesser GNU Public License"
+  :license "MIT License"
   :components ((:file "runtime")
                (:file "unification" :depends-on ("runtime"))
                (:file "st-types")
--- unification.lisp
+++ unification.lisp
@@ -1,9 +1,22 @@
-;; This software is Copyright (c) Ivan Boldyrev 2004
-;; Ivan Boldyrev grants you the rights to distribute
-;; and use this software as governed by the terms
-;; of the Lisp Lesser GNU Public License
-;; (http://opensource.franz.com/preamble.html),
-;; known as the LLGPL.
+;; Copyright (c) 2004, 2022 Ivan Boldyrev
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
 
 ;;;  unification as solution of set of equations
 
